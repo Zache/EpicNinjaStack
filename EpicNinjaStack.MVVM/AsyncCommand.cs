8ec69379b0fbfff56d9f00a41d21dc3b3c78e00e
Zache <zacharias.bjorngren@gmail.com>
2014-06-13 10:14:36 +0200

    A basic ICommand implementation

---
+++ EpicNinjaStack.MVVM/AsyncCommand.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EpicNinjaStack.MVVM
+{
+	public class AsyncCommand : BaseCommand
+	{
+		private readonly Func<Task> _execute;
+		private readonly Func<bool> _canExecute;
+
+		private bool _isExecuting;
+
+		public AsyncCommand(Func<Task> execute, Func<bool> canExecute)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public async Task ExecuteAsync(object parameter = null)
+		{
+			try
+			{
+				_isExecuting = true;
+
+				await _execute();
+			}
+			finally
+			{
+				_isExecuting = false;
+				CommandManager.InvalidateRequerySuggested();
+			}
+		}
+
+		public override bool CanExecute(object parameter = null)
+		{
+			return !_isExecuting && _canExecute();
+		}
+
+		public async override void Execute(object parameter = null)
+		{
+			await ExecuteAsync(parameter);
+		}
+	}
+}
---
+++ EpicNinjaStack.MVVM/AsyncCommandT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EpicNinjaStack.MVVM
+{
+	public class AsyncCommand<T> : BaseCommand
+	{
+		private readonly Func<T, Task> _execute;
+		private readonly Func<bool> _canExecute;
+
+		private bool _isExecuting;
+
+		public AsyncCommand(Func<T, Task> execute, Func<bool> canExecute)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public async Task ExecuteAsync(T parameter)
+		{
+			try
+			{
+				_isExecuting = true;
+
+				await _execute(parameter);
+			}
+			finally
+			{
+				_isExecuting = false;
+				CommandManager.InvalidateRequerySuggested();
+			}
+		}
+
+		public override bool CanExecute(object parameter = null)
+		{
+			return !_isExecuting && _canExecute();
+		}
+
+		public async override void Execute(object parameter)
+		{
+			await ExecuteAsync((T)parameter);
+		}
+	}
+}
