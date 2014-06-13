fd26dc172b8d15d4a4c3a9f10e128337e090d73d
Zache <zacharias.bjorngren@gmail.com>
2014-06-13 14:29:51 +0200

    Edit person

---
+++ EpicNinjaStack.MVVM/Converters/PrefixConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicNinjaStack.MVVM.Converters
+{
+	public class PrefixConverter : BaseConverter
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var valueAsString = value as string;
+			var paramAsString = parameter as string;
+
+			return paramAsString + " " + valueAsString;
+		}
+
+		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
